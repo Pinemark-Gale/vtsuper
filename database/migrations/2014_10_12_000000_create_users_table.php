521e416e7586bbed2924989ed934b203483f665e
Pinemark-Gale <gale@pinemarkstudio.com>
2021-09-02 18:05:47 -0400

    Added foreign key contraints to User object.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('privilege_id');
-            $table->foreignId('school_id')->nullable();
+            $table->foreignId('privilege_id')->constrained();
+            $table->foreignId('school_id')->contrained()->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
