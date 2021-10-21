285048c8b6d9696a57a6ca0054fd69f730640032
Pinemark-Gale <gale@pinemarkstudio.com>
2021-10-20 21:16:59 -0400

    Enhanced welcome screen rocket styling.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,12 @@
             <h1>Students</h1>
         </div>
     </div>
-    <div class="canvas">
-        <div class="section-left">
-            <h1 class="title">How to Use VT-SUPER</h1>
-            <h2 class="sub-title">Resources for Educators & Students</h2>
-            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p class="text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occae- cat cupidatat non proident, sunt in culpa qui officia dese- runt mollit anim id est laborum.</p>
-            <p class="text">Cras id quam ac leo dignissim sodales id eu est. Maecenas tincidunt nisi mi, in facilisis sapien tincidunt placerat. Ut dapibus finibus justo, id pulvinar est accumsan sed. Mauris vehicula odio non ipsum maximus condimentum.</p>
-        </div>
-        <x-svg.rocket-planet-image class="home-page-rocket" />
+    <div class="rocket-canvas">
+        <h1 class="title">How to Use VT-SUPER</h1>
+        <h2 class="sub-title">Resources for Educators & Students</h2>
+        <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p class="text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occae- cat cupidatat non proident, sunt in culpa qui officia dese- runt mollit anim id est laborum.</p>
+        <p class="text">Cras id quam ac leo dignissim sodales id eu est. Maecenas tincidunt nisi mi, in facilisis sapien tincidunt placerat. Ut dapibus finibus justo, id pulvinar est accumsan sed. Mauris vehicula odio non ipsum maximus condimentum.</p>
     </div>
     <div class="section-right">
         <h1 class="title">Why VT-SUPER</h1>
